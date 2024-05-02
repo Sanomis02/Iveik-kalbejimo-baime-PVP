4b22742bfb65af022634c4285f2bd75449a2568a
Simonas Skucas <98412351+Sanomis02@users.noreply.github.com>
2024-05-02 16:08:37 +0300

    Menu ir build settings
    Įdėti meniu į vaiko kambarį, pakeisti build setting'ai

---
+++ Iveik-kalbejimo-baime-VR/Assets/Scripts/Environment/ZaidimoMenuController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ZaidimoMenuController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
--- Iveik-kalbejimo-baime-VR/Assets/VRTemplateAssets/Scripts/StepManager.cs
+++ Iveik-kalbejimo-baime-VR/Assets/VRTemplateAssets/Scripts/StepManager.cs
@@ -33,7 +33,7 @@ namespace Unity.VRTemplate
             m_StepList[m_CurrentStepIndex].stepObject.SetActive(false);
             m_CurrentStepIndex = (m_CurrentStepIndex + 1) % m_StepList.Count;
             m_StepList[m_CurrentStepIndex].stepObject.SetActive(true);
-            m_StepButtonTextField.text = m_StepList[m_CurrentStepIndex].buttonText;
+            m_StepButtonTextField.text = "Kitas";
         }
     }
 }
