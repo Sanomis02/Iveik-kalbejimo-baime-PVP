bade3cfe894fe32a4a93186dd4c8a0a920f996bf
domrin <domrin@ktu.lt>
2024-05-23 21:21:56 +0300

    Misko scenos update su rankom ir titrais
    Misko scenos update su rankom ir titrais

--- Iveik-kalbejimo-baime-VR/Assets/Scripts/NPC/SubtitlesMove.cs
+++ Iveik-kalbejimo-baime-VR/Assets/Scripts/NPC/SubtitlesMove.cs
@@ -10,7 +10,7 @@ public class SubtitlesMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       // this.sub = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
